4e4c287061bbcb8da020744c0ce37b6c524c38fd
danielkarling <danielkarling@danielkarling>
2017-11-07 17:16:08 -0200

    Medias e aprovacao

--- classes/Dificuldades.php
+++ classes/Dificuldades.php
@@ -33,16 +33,7 @@ class Dificuldades {
                         <tr class="text-center bg-warning"> 
                             <td  class="text-success" ><?php echo $this->categorias[$index1]->getNome(); ?></td>
                             <td  class="text-success" ><?php echo $this->difs[$index1]; ?></td>
-                            <td  class="text-success">
-                                <?php
-                                $grafico = new graficoPizza();
-                                $valor = $this->difs[$index1];
-                                $legenda = $this->categorias[$index1]->getNome() . "";
-                                $grafico->setValor($valor);
-                                $grafico->setLegenda($legenda);
-                                $grafico->desenhar();
-                                ?>
-                            </td>
+
                         </tr>
                         <?php
                     }
@@ -55,30 +46,65 @@ class Dificuldades {
 
     public function desenhaGraficos() {
         ?>
-        <div class="container-fluid">
+        <div class="container-fluid bg-warning">
 
-            <table class="table table-striped">
-                <tr class="text-center bg-warning"> 
+            <table class="table table-bordered">
+                <div class="row">
                     <?php
                     for ($index = 0; $index < count($this->categorias); $index++) {
                         ?>
-                        <td class = "text-success" >
-                            <?php
-                            $grafico = new graficoPizza();
-                            $valor = $this->difs[$index];
-                            $legenda = $this->categorias[$index]->getNome() . "";
-                            $grafico->setValor($valor);
-                            $grafico->setLegenda($legenda);
-                            $grafico->setTitulo("Dificuldade em " . $legenda);
-                            $grafico->desenhar();
-                            ?>
-                        </td>
+                        <tr class="text-center bg-warning"> 
+                            <td class = "text-success" >
+                                <?php
+                                $grafico = new graficoPizza();
+                                $valor = $this->difs[$index];
+                                $legenda = $this->categorias[$index]->getNome() . "";
+                                $grafico->setValor($valor);
+                                $grafico->setLegenda($legenda);
+                                $grafico->setTitulo("Dificuldade em " . $legenda);
+                                $grafico->desenhar();
+                                ?>
+                            </td>
+                        </tr>
 
                         <?php
                     }
                     ?>
-                </tr>
+                </div>
             </table>
+            <?php
+        }
+
+        public function imprimeMediaAprovacao() {
+            ?>
+            <center>
+
+
+                <div class="container-fluid">
+
+                    <table class="table table-striped">
+                        <tr class="text-center">
+                            <td class="alert-info"> Categoria </td>
+                            <td class="alert-info"> % Aprovação </td>
+                            <td class="alert-info"> Média </td>
+                        </tr>
+                        <?php
+                        for ($index1 = 0; $index1 < count($this->difs); $index1++) {
+                            if($this->categorias[$index1]->getQtd()>0){
+                            ?>   
+                            <tr class="text-center bg-warning"> 
+                                <td  class="text-success" ><?php echo $this->categorias[$index1]->getNome(); ?></td>
+                                <td  class="text-success" ><?php echo $this->categorias[$index1]->getPercentAprovacao(); ?></td>
+                                <td  class="text-success" ><?php echo $this->categorias[$index1]->getMediaFinal(); ?></td>
+
+                            </tr>
+                            <?php
+                            }
+                        }
+                        ?>
+                    </table>
+                </div>
+            </center>
             <?php
         }
 
